8a23cd2e7bb7a0c221bb84fecfec6ddd2c2f1193
alexandercoabad <144318238+alexandercoabad@users.noreply.github.com>
2026-06-26 05:59:44 +0800

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_AbAdA_2048 user_project (
+  tt_um_combined user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
